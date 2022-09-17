203beb3248f3060ae41e2f23bc8653904468fd6c
Sourov Kar <84342803+sourovkar@users.noreply.github.com>
2022-09-16 20:36:12 -0400

    Updated HTML anD CSS

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ let paverSqft = (paverWidthInch/12) * (paverLengthInch/12);
 let paverPriceDollar = 0.5;
 
 function calculateBtn() {
-let lotWidthFeet = document.getElementById("lot-width").value;
-let lotLengthFeet = document.getElementById("lot-length").value;
-let lotSqft = lotWidthFeet * lotLengthFeet;
+    let lotWidthFeet = document.getElementById("lot-width").value;
+    let lotLengthFeet = document.getElementById("lot-length").value;
+    let lotSqft = lotWidthFeet * lotLengthFeet;
 
-let paverQty = Math.ceil(lotSqft/paverSqft);
-let totalCost = paverPriceDollar * paverQty;
+    let paverQty = Math.ceil(lotSqft/paverSqft);
+    let totalCost = paverPriceDollar * paverQty;
 
 
-let result = `Hello! ${paverQty} pavers are necessary to cover ${lotSqft} sqft area.
-Total cost will be ${totalCost} dollars.`;
+    let result = `Hello! ${paverQty} pavers are necessary to cover ${lotSqft} sqft area.
+    Total cost will be ${totalCost} dollars.`;
 
-document.getElementById("output").textContent = result;
+    document.getElementById("output").textContent = result;
 
 }
